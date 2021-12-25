9bbc29994332b2c93b809a54f5ef14d58afebbfd
teja548 <tet9692@gmail.com>
2021-12-25 23:07:03 +0530

    image-cursor

---
+++ assessment/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import cursorImg from './cursor.png';
+
+const useMousePosition = () => {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMousePosition = event => {
+      setMousePos({ x: event.clientX, y: event.clientY });
+    };
+    window.addEventListener('mousemove', handleMousePosition);
+    return () => window.removeEventListener('mousemove', handleMousePosition);
+  }, []);
+
+  return mousePos;
+};
+
+const ImageCursor = () => {
+  const { x, y } = useMousePosition();
+
+  return (
+    <div>
+      <img src={cursorImg} width={100} alt='Cursor' style={{ transform: `translate(${x}px,${y}px) ` }} />
+    </div>
+  );
+};
+
+export default ImageCursor;
